0959d5a90a6fed1fc79b3445934af28a1a838704
MD Nelles <devstudioca@gmail.com>
2023-01-24 18:45:31 -0500

    db connect cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,35 +8,19 @@ import { DetailsSchema } from './schema/details.schema';
 /* eslint-disable */
 const env = require('dotenv').config().parsed;
 
-const uri = `mongodb+srv://${env.NODE_JS_MONGO_USER}:${env.NODE_JS_MONGO_PASSWORD}@{env.NODE_JS_MONGO_HOST}/?retryWrites=true&w=majority`;
-
-/*
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});*/
-
+const uri = `mongodb+srv://${env.NODE_JS_MONGO_USER}:${env.NODE_JS_MONGO_PASSWORD}@${env.NODE_JS_MONGO_HOST}/?retryWrites=true&w=majority`;
 
 @Module({
   imports: [
-    MongooseModule.forRoot(uri,
-      { dbName: env.NODE_JS_MONGO_DB_NAME, useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 },
-    ),
+    MongooseModule.forRoot(uri, {
+      dbName: env.NODE_JS_MONGO_DB_NAME,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    }),
     MongooseModule.forFeature([{ name: 'Details', schema: DetailsSchema }]),
   ],
   controllers: [AppController, LakesController],
   providers: [AppService],
-});
-
+})
 export class AppModule {}
-
-/*
-@Module({
-  imports: [
-    MongooseModule.forRoot(uri),
-    MongooseModule.forFeature([{ name: 'Details', schema: DetailsSchema }]),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})*/
