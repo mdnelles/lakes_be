7e251073f004a54b25179d6c65c129094d4e693d
MD Nelles <devstudioca@gmail.com>
2023-01-24 09:59:28 -0500

    update mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LakesController } from './lakes/lakes.controller';
+const env = require('dotenv').config().parsed;
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb://${env.NODE_JS_MONGO_USER}:${env.NODE_JS_MONGO_PASSWORD}@${env.NODE_JS_MONGO_HOST}/?retryWrites=true&w=majority`,
+      { dbName: 'LakesWorld' },
+    ),
+  ],
   controllers: [AppController, LakesController],
   providers: [AppService],
 })
